53d19981df4015b02d433ac052e7dc653163546c
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-29 19:26:12 -0500

    feat: Se finalizo pantalla de HistoryScreen

--- src/screens/HistoryScreen.jsx
+++ src/screens/HistoryScreen.jsx
@@ -1,24 +1,29 @@
 import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { useWater } from "../context/WaterContext";
 import COLORS from "../constants/colors";
 
 export default function HistoryScreen() {
+
+    const { water, goal } = useWater();
+    const percentage = Math.round((water / goal) * 100);
+
     return (
         <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Historial</Text>
 
         <View style={styles.card}>
             <Text style={styles.label}>Consumo de hoy</Text>
-            <Text style={styles.value}>1200 ml</Text>
+            <Text style={styles.value}>{water} ml</Text>
         </View>
 
         <View style={styles.card}>
             <Text style={styles.label}>Meta diaria</Text>
-            <Text style={styles.value}>2000 ml</Text>
+            <Text style={styles.value}>{goal} ml</Text>
         </View>
 
         <View style={styles.card}>
             <Text style={styles.label}>Progreso</Text>
-            <Text style={styles.value}>60%</Text>
+            <Text style={styles.value}>{percentage}%</Text>
         </View>
         </SafeAreaView>
     );
